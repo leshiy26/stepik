a12a455c3f68a467b08bf3530a029184e63343d2
Олег Каминский <kamo@automacon.ru>
2023-09-28 19:16:43 +0300

    https://stepik.org/lesson/228838/step/1?unit=201372

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+/*
+Из натурального числа удалить заданную цифру.
+*/
 func main() {
+	var N, t, u int
+	fmt.Scanln(&N)
+	fmt.Scanln(&u)
+	a := make([]int, 0, 100)
+	//	fmt.Printf("Тип: %T, Значени: %v", a, a)
+	for N > 0 {
+		t = N % 10
+		if t != u {
+			a = append(a, t)
+		}
+		N = N / 10
+	}
+	for i := len(a); i >= 1; i-- {
+		fmt.Printf("%v", a[i-1])
+	}
 
 }
