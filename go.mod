50c7f649e66f7a804c6a4f49ec1361751588af85
Олег Каминский <kamo@automacon.ru>
2023-10-18 08:43:35 +0300

    add mod

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module stepik
 
 go 1.21.1
+
+require github.com/google/go-cmp v0.6.0
+
+require github.com/leshiy26/stepik/module2 v0.0.0-20231013202216-edceb4f20254 // indirect
--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"stepik/morestrings"
 
-	"github.com/leshiy26/stepik/module2"
+	"github.com/google/go-cmp/cmp"
+	"github.com/leshiy26/stepik/tree/main/module2"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 	fmt.Println(module2.TestModule2())
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
